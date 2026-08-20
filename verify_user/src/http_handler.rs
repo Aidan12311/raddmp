d2464a8d37bc30c4ad4265a07ae96db4b443bbdb
ImAPersonIght <zomhorsethompson2656@gmail.com>
2026-08-20 14:43:25 -0700

    update user-branch

--- verify_user/src/http_handler.rs
+++ verify_user/src/http_handler.rs
@@ -1,5 +1,7 @@
 use lambda_http::{Body, Error, Request, RequestExt, Response};
 
+use crate::helpers::text_response;
+
 //check query parameters for token
 //decode jwt
 //query the database for user id
@@ -7,6 +9,10 @@ use lambda_http::{Body, Error, Request, RequestExt, Response};
 //post update to database
 pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let path_params = event.path_parameters();
-    let auth_token = path_parameters.first("token").unwrap_or_default("");
+    let auth_token = path_params.first("token").unwrap_or("");
+
+    if auth_token.is_empty() {
+        return text_response("Verification token missing", 400)
+    }
     todo!()
 }
